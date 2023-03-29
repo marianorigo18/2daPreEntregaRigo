e795f98df6efffde2c5d0346eeb6988688ace975
marianorigo18 <marianowagnerrigo@gmail.com>
2023-03-29 18:54:29 -0300

    01:00:03

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import NavBar from "./components/NavBar/NavBar";
 function App() {
   return (
     <BrowserRouter>
+    <NavBar></NavBar>
       <Routes>
         <Route path="/" element={<ItemListContainer/>}></Route>
         <Route path="/category/:categoryId" element={<ItemListContainer/>}></Route>
